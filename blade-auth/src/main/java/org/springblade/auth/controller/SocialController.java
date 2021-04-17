30ab24745091b4d970e471085d9f2d87327fc0eb
jiajingfei <15131066253@163.com>
2021-04-17 19:42:52 +0800

    贾晟哲新增一个方法

--- blade-auth/src/main/java/org/springblade/auth/controller/SocialController.java
+++ blade-auth/src/main/java/org/springblade/auth/controller/SocialController.java
@@ -88,5 +88,15 @@ public class SocialController {
 		return authRequest.refresh(AuthToken.builder().refreshToken(token).build());
 	}
 
+	/**
+	 * 续期accessToken
+	 */
+	@ApiOperation(value = "续期令牌")
+	//@RequestMapping("/oauth/refresh/{source}")
+	public Object refreshAuth3(@PathVariable("source") String source, String token) {
+		AuthRequest authRequest = SocialUtil.getAuthRequest(source, socialProperties);
+		return authRequest.refresh(AuthToken.builder().refreshToken(token).build());
+	}
+
 
 }
